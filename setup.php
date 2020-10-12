a80ca9f53048f81931fbf38f7731cdfbf2ad7bec
Alberto Sosa <petox.somart@outlook.com>
2020-10-11 20:38:34 -0500

    cambios en la agregación de productos

--- setup.php
+++ setup.php
@@ -40,19 +40,18 @@
 
     queryMysql("INSERT INTO products (name, price, stock, description, detalles, img)
     VALUES('Silla', '300', '3', 'Silla antigüa en buen estado',
-    'Silla del siglo XIII en muy buen estado', 'silla.png')
-    WHERE NOT EXISTS(SELECT * FROM products WHERE name = )");
+    'Silla del siglo XIII en muy buen estado', 'silla.png')");
 
-    queryMysql("INSERT INTO products (name, price, stock, description, detalles, img, button)
+    queryMysql("INSERT INTO products (name, price, stock, description, detalles, img)
     VALUES('Objetos varios', '1000', '1', 'Objetos del siglo XV, se vende todo junto',
     'Hay jarrones, un escritorio, lámparas, una maquina de escribir y una pintura', 'anti.png')");
 
-    queryMysql("INSERT INTO products (name, price, stock, description, detalles, img, button)
+    queryMysql("INSERT INTO products (name, price, stock, description, detalles, img)
     VALUES('Sofa', '750', '3', 'Sofá antiguo del siglo XIII restaurado',
     'Es un sofá de 2 x 1.5 metros', 'sofa.png')");
 
     queryMysql("INSERT INTO members(user, pass, idaccess, money)
-    VALUES('admin', '123', '1', '6000')");
+    VALUES('admin', '123', '1', '')");
 
 ?>
 
